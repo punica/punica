5ef288e47999aa8af6a93a3dca5aa113102278d9
Tomas Markevičius <tomas.m@8devices.com>
2018-12-10 13:48:52 +0200

    Improve path handling of style format checker

--- script/format_style.sh
+++ script/format_style.sh
@@ -2,30 +2,37 @@
 
 set -e
 
-ASTYLE=./astyle/build/gcc/bin/astyle
-ASTYLE_OPTS="--options=.astyle.conf $@"
-ASTYLE_FILES="src/*.h src/*.c"
+TMP_BUILD_DIR="$(mktemp -d)"
+PROJECT_ROOT_DIR="$(cd $(dirname "$0")/.. && pwd -P)"
 
-# Download and install
-if [ ! -x $ASTYLE ]; then
-    wget https://sourceforge.net/projects/astyle/files/astyle/astyle%203.1/astyle_3.1_linux.tar.gz
-    tar xzvf astyle_3.1_linux.tar.gz
-    cd astyle/build/gcc
+ASTYLE="${PROJECT_ROOT_DIR}/astyle"
+ASTYLE_OPTS="--options=${PROJECT_ROOT_DIR}/.astyle.conf $@"
+ASTYLE_FILES="${PROJECT_ROOT_DIR}/src/*.h ${PROJECT_ROOT_DIR}/src/*.c"
+ASTYLE_URL="https://sourceforge.net/projects/astyle/files/astyle/astyle%203.1/astyle_3.1_linux.tar.gz"
+ASTYLE_ARCHIVE="${TMP_BUILD_DIR}/astyle.tar.gz"
+ASTYLE_DIR="${TMP_BUILD_DIR}/astyle"
+
+get_astyle () {
+    eval "wget ${ASTYLE_URL} -O ${ASTYLE_ARCHIVE}"
+    eval "tar xzvf ${ASTYLE_ARCHIVE} --directory ${TMP_BUILD_DIR}/"
+    eval "cd ${ASTYLE_DIR}/build/gcc"
     make
-    cd ../../..
-fi
+    eval "mv ${ASTYLE_DIR}/build/gcc/bin/astyle ${ASTYLE}"
+    cd -
+}
 
-# Verify version
-$ASTYLE --version
-if [ "`$ASTYLE --version`" != "Artistic Style Version 3.1" ]; then
+if [ ! -x $ASTYLE ]; then
+    get_astyle | rm -rf ${TMP_BUILD_DIR}; exit 1
+fi
+rm -rf ${TMP_BUILD_DIR}
+
+if [ `eval "${ASTYLE} --version"` != "Artistic Style Version 3.1" ]; then
     echo "Error! Astyle version must be 3.1!"
     exit 1
 fi
 
-# Check style
-echo "$ASTYLE $ASTYLE_OPTS $ASTYLE_FILES"
-output=`$ASTYLE $ASTYLE_OPTS $ASTYLE_FILES`
-if [ -z "$output" ]; then
+output=`eval "${ASTYLE} ${ASTYLE_OPTS} ${ASTYLE_FILES}"`
+if [ -z "${output}" ]; then
     echo "Code style pass!"
 else
     echo "Code style fail!"
